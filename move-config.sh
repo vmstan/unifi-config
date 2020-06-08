0a87d5f34be530d2dbcc77f4ec25ef13324526de
Michael Stanclift <mstanclift@vmware.com>
2020-06-08 12:07:36 -0500

    sudo chown

--- move-config.sh
+++ move-config.sh
@@ -2,5 +2,5 @@
 
 echo -e "Moving config.properties.json in place"
 sudo cp config.properties.json /var/lib/unifi/sites/default/config.properties.json
-chown unifi:unifi config.properties.json
+sudo chown unifi:unifi config.properties.json
 echo -e "Force provision to complete"

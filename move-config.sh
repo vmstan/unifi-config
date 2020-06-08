b1bdba6bd5b4394543094238f4126f1a72c65387
Michael Stanclift <mstanclift@vmware.com>
2020-06-08 12:27:41 -0500

    pull updates

--- move-config.sh
+++ move-config.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 
+git pull
 echo -e "Moving config.properties.json in place"
 sudo cp config.properties.json /var/lib/unifi/sites/default/config.properties.json
 sudo chown unifi:unifi config.properties.json

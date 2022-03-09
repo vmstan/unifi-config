009f32c7d8d5fee297126b7fb0484aa90d73ca2c
Michael Stanclift <mx@vmstan.com>
2022-03-09 11:29:22 -0600

    get all the things

---
+++ getwogit.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function get_dcupdater() {
+    echo -e "Downloading Docker Compose Updater"
+    curl -s -L --create-dirs -o ./update-compose.sh "https://raw.githubusercontent.com/vmstan/unifi-config/master/update-compose.sh?token=GHSAT0AAAAAABQTASPZ34CHL3FDJ5TR7ANMYRSDGAQ"
+    chmod +x update-compose.sh
+}
+
+function get_dcbedrock() {
+    echo -e "Downloading Bedrock Docker Compose"
+    curl -s -L --create-dirs -o ./bedrock/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/mc-bedrock/docker-compose.yml?token=GHSAT0AAAAAABQTASPZNHPYMUSEM6TPASP6YRSDIBA"
+}
+
+function get_dcminecraft() {
+    echo -e "Downloading Minecraft Java Docker Compose"
+    curl -s -L --create-dirs -o ./minecraft/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/mc-java/docker-compose.yml?token=GHSAT0AAAAAABQTASPZGGGG6NWRVFWUGITIYRSDMFQ"
+}
+
+function get_dcdnsproxy() {
+    echo -e "Downloading DNSPROXY Docker Compose"
+    curl -s -L --create-dirs -o ./dnsproxy/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/dnsproxy/docker-compose.yml?token=GHSAT0AAAAAABQTASPYA2ZK7BG5TUEI77ZIYRSDLFQ"
+}
+
+function get_dchomebridge() {
+    echo -e "Downloading Homebridge Docker Compose"
+    curl -s -L --create-dirs -o ./homebridge/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/homebridge/docker-compose.yml?token=GHSAT0AAAAAABQTASPZN7EYNRWFZOP3J7YUYRSDOGA"
+}
+
+function get_dcpihole() {
+    echo -e "Downloading Pihole Docker Compose"
+    curl -s -L --create-dirs -o ./pihole/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/pihole/docker-compose.yml?token=GHSAT0AAAAAABQTASPZAFRUGWUQWWWFAZW4YRSDOWQ"
+}
+
+get_dcupdater
+get_dcbedrock
+get_dcminecraft
+get_dcdnsproxy
+get_dchomebridge
+get_dcpihole

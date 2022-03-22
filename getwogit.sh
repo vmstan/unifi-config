300a2312517c9d58a7af98717a5cd358f6563196
Michael Stanclift <mx@vmstan.com>
2022-03-22 09:50:20 -0500

    clean up

--- getwogit.sh
+++ getwogit.sh
@@ -2,38 +2,38 @@
 
 function get_dcupdater() {
     echo -e "Downloading Docker Compose Updater"
-    curl -s -L --create-dirs -o ./update-compose.sh "https://raw.githubusercontent.com/vmstan/unifi-config/master/update-compose.sh?token=GHSAT0AAAAAABQTASPZ34CHL3FDJ5TR7ANMYRSDGAQ"
+    curl -s -L --create-dirs -o ./update-compose.sh "https://raw.githubusercontent.com/vmstan/unifi-config/master/update-compose.sh"
     chmod +x update-compose.sh
 }
 
 function get_dcbedrock() {
     echo -e "Downloading Bedrock Docker Compose"
-    curl -s -L --create-dirs -o ./bedrock/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/mc-bedrock/docker-compose.yml?token=GHSAT0AAAAAABQTASPZNHPYMUSEM6TPASP6YRSDIBA"
+    curl -s -L --create-dirs -o ./bedrock/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/mc-bedrock/docker-compose.yml"
 }
 
 function get_dcminecraft() {
     echo -e "Downloading Minecraft Java Docker Compose"
-    curl -s -L --create-dirs -o ./minecraft/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/mc-java/docker-compose.yml?token=GHSAT0AAAAAABQTASPZGGGG6NWRVFWUGITIYRSDMFQ"
+    curl -s -L --create-dirs -o ./minecraft/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/mc-java/docker-compose.yml"
 }
 
-function get_dcdnsproxy() {
+function get_dcdnsproxy() { 
     echo -e "Downloading DNSPROXY Docker Compose"
-    curl -s -L --create-dirs -o ./dnsproxy/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/dnsproxy/docker-compose.yml?token=GHSAT0AAAAAABQTASPYA2ZK7BG5TUEI77ZIYRSDLFQ"
+    curl -s -L --create-dirs -o ./dnsproxy/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/dnsproxy/docker-compose.yml"
 }
 
 function get_dchomebridge() {
     echo -e "Downloading Homebridge Docker Compose"
-    curl -s -L --create-dirs -o ./homebridge/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/homebridge/docker-compose.yml?token=GHSAT0AAAAAABQTASPZN7EYNRWFZOP3J7YUYRSDOGA"
+    curl -s -L --create-dirs -o ./homebridge/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/homebridge/docker-compose.yml"
 }
 
 function get_dcpihole() {
     echo -e "Downloading Pihole Docker Compose"
-    curl -s -L --create-dirs -o ./pihole/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/pihole/docker-compose.yml?token=GHSAT0AAAAAABQTASPZAFRUGWUQWWWFAZW4YRSDOWQ"
+    curl -s -L --create-dirs -o ./pihole/docker-compose.yml "https://raw.githubusercontent.com/vmstan/unifi-config/master/docker/pihole/docker-compose.yml"
 }
 
 function get_unboundconf() {
     echo -e "Downloading Unbound Configuration"
-    curl -s -L --create-dirs -o /usr/local/etc/unbound/unbound.conf "https://raw.githubusercontent.com/vmstan/unifi-config/master/unbound/unbound.conf?token=GHSAT0AAAAAABQTASPYUEL7IPXJ5U6N66LCYRSD4SA"
+    curl -s -L --create-dirs -o /usr/local/etc/unbound/unbound.conf "https://raw.githubusercontent.com/vmstan/unifi-config/master/unbound/unbound.conf"
 }
 
 get_dcupdater
